a7b45adb20b29b74c345f618d0a7c0b3ca1c712e
Zulk <65410126+Zulkifli70@users.noreply.github.com>
2025-05-30 20:08:20 +0700

    Complete reverse string challenge

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  return input.split("").reverse().join("");
 };
+
+console.log("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
