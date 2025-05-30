98358e055f5974754d37b7aa2ba1a2872a9a66a8
Zulk <65410126+Zulkifli70@users.noreply.github.com>
2025-05-30 19:38:10 +0700

    Complete challenge repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (greet, length) {
+  if (length < 0) {
+    return "ERROR";
+  }
 
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += greet;
+  }
+  return result;
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
