885e857dc76943e432fb18a7b8b6eb9093da0cbe
Zulk <65410126+Zulkifli70@users.noreply.github.com>
2025-06-01 13:28:30 +0700

    Complete challenge removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...itemToRemove) {
+  const array2 = [...array];
+
+  return array2.filter((item) => !itemToRemove.includes(item));
 };
 
 // Do not edit below this line
