14b85d6f95623b28792dbc85e00370ef562bc084
Zulk <65410126+Zulkifli70@users.noreply.github.com>
2025-06-09 13:02:50 +0700

    Complete challenge palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let removeChar = string.replace(/[^A-Z0-9]/gi, "").toLowerCase();
+  let check = removeChar.split("").reverse().join("");
 
+  return removeChar === check;
 };
 
 // Do not edit below this line
