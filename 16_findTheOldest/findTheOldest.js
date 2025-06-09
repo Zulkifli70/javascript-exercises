075d3ddc397c67a8cef461d342e59a73be03bd31
Zulk <65410126+Zulkifli70@users.noreply.github.com>
2025-06-09 14:20:32 +0700

    complete findTheOldest Challenge

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce(function (a, b) {
+    const lastPeople =
+      a.yearOfDeath !== undefined
+        ? a.yearOfDeath - a.yearOfBirth
+        : new Date().getFullYear() - a.yearOfBirth;
+    const nextPeople =
+      b.yearOfDeath !== undefined
+        ? b.yearOfDeath - b.yearOfBirth
+        : new Date().getFullYear() - b.yearOfBirth;
+    return lastPeople > nextPeople ? a : b;
+  });
 };
 
 // Do not edit below this line
