14c99bba3b87f86e8a0be32d6c05e9c575e1eb8e
Zulk <65410126+Zulkifli70@users.noreply.github.com>
2025-06-09 13:15:50 +0700

    Complete fibonacci challenge

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  let integer = parseInt(number);
+  if (integer < 0) return "OOPS";
+  if (integer <= 1) return integer;
+  return fibonacci(integer - 1) + fibonacci(integer - 2);
 };
 
 // Do not edit below this line
